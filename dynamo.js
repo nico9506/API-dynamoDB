3ac3466fbd299c7302d847ec02d4d9bcdb2e8f8a
Nicolas Lopez <niclopezt@gmail.com>
2024-04-15 17:02:49 +1000

    Add CRUD methods

--- dynamo.js
+++ dynamo.js
@@ -11,8 +11,7 @@ AWS.config.update({
 });
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
-// const TABLE_NAME = "harry_potter-api";
-const TABLE_NAME = "Music";
+const TABLE_NAME = "harry_potter_api";
 
 const getCharacters = async () => {
     const params = {
@@ -23,4 +22,75 @@ const getCharacters = async () => {
     return characters;
 };
 
-getCharacters();
+const getCharacterById = async (id) => {
+    const params = {
+        TableName: TABLE_NAME,
+        Key: {
+            id,
+        },
+    };
+
+    return await dynamoClient.get(params).promise();
+};
+
+const addOrUpdateCharacter = async (character) => {
+    const params = {
+        TableName: TABLE_NAME,
+        Item: character,
+    };
+
+    return await dynamoClient.put(params).promise();
+};
+
+const deleteCharacter = async (id) => {
+    const params = {
+        TableName: TABLE_NAME,
+        Key: {
+            id,
+        },
+    };
+
+    return await dynamoClient.delete(params).promise();
+};
+
+module.exports = {
+    dynamoClient,
+    getCharacters,
+    getCharacterById,
+    addOrUpdateCharacter,
+    deleteCharacter,
+};
+
+// getCharacters();
+
+// const hp = {
+//     id: "0",
+//     name: "Harry Potter",
+//     alternate_names: [
+//         "The Boy Who Lived",
+//         "The Chosen One",
+//         "Undesirable No. 1",
+//         "Potty",
+//     ],
+//     species: "human",
+//     gender: "male",
+//     house: "Gryffindor",
+//     dateOfBirth: "31-07-1980",
+//     yearOfBirth: 1980,
+//     wizard: true,
+//     ancestry: "half-blood",
+//     eyeColour: "green",
+//     hairColour: "black",
+//     wand: { wood: "holly", core: "phoenix tail feather", length: 11 },
+//     patronus: "stag",
+//     hogwartsStudent: true,
+//     hogwartsStaff: false,
+//     actor: "Daniel Radcliffe",
+//     alternate_actors: [],
+//     alive: true,
+//     image: "https://ik.imagekit.io/hpapi/harry.jpg",
+// };
+
+// addOrUpdateCharacter(hp);
+
+// deleteCharacter("0");

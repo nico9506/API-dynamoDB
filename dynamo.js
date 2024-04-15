d54b72dc65b9380940676357cd4baee806b807cf
Nicolas Lopez <niclopezt@gmail.com>
2024-04-15 15:19:00 +1000

    Connect to a local dynamoDB table

--- dynamo.js
+++ dynamo.js
@@ -5,10 +5,14 @@ AWS.config.update({
     region: process.env.AWS_DEFAULT_REGION,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    endpoint: "http://localhost:8000",
+    sslEnabled: false,
+    s3ForcePathStyle: true,
 });
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
-const TABLE_NAME = "harrypotter-api";
+// const TABLE_NAME = "harry_potter-api";
+const TABLE_NAME = "Music";
 
 const getCharacters = async () => {
     const params = {
